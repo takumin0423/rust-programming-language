b8435b8fcd4bedadfc1ccc28f3d25193fe0b1604
takumin0423 <takumin0423baske@gmail.com>
2024-06-30 14:30:25 +0900

    パースとコマンドの実行

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,10 +1,26 @@
 fn main() {
     for line in std::io::stdin().lines() {
+        let mut stack = vec![];
+
         if let Ok(line) = line {
             let words: Vec<_> = line.split(" ").collect();
 
-            println!("Line: {words:?}")
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
         }
+
+        println!("stack: {stack:?}");
     }
 }
 
@@ -13,4 +29,25 @@ fn add(stack: &mut Vec<i32>) {
     let rhs = stack.pop().unwrap();
 
     stack.push(lhs + rhs);
+}
+
+fn sub(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+
+    stack.push(lhs - rhs);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+
+    stack.push(lhs * rhs);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+
+    stack.push(lhs / rhs);
 }
